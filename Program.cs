5f9d880372068e94a37a15e7c2ea10f04e12e397
Sebastian Lozza <sebastianhlozza@hotmail.com>
2022-09-06 14:13:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Introduce tu nombre:");
+string nombre = Console.ReadLine();
+string datetime = DateTime.Now.ToString("hh:mm:ss");
+Console.WriteLine  ($"Hello, {nombre} Its {datetime} in Argentina");
+
+
